641b3544b020aea8ec38d8eb8e041344e2823b44
hgoren19 <64122626+hgoren19@users.noreply.github.com>
2020-04-24 15:29:47 -0500

    Update AllGood.java

--- src/day_1/AllGood.java
+++ src/day_1/AllGood.java
@@ -3,5 +3,8 @@ package day_1;
 public class AllGood {
     public static void main(String[] args) {
         System.out.println("Everything is going to be alright.");
+        
+        // This line is added from GitHub.
+        
     }
 }
