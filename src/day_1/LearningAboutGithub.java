756d9391cfc6602e08c893c4e23b47f49004c1bd
hgoren19 <hgoren19@gmail.com>
2020-04-24 14:54:39 -0500

    add LearningAboutGithub.java

---
+++ src/day_1/LearningAboutGithub.java
@@ -0,0 +1,7 @@
+package day_1;
+
+public class LearningAboutGithub {
+    public static void main(String[] args) {
+        System.out.println("Today we are learning how to push the code to Github");
+    }
+}
